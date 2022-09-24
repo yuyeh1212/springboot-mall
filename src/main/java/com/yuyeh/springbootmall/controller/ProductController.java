9a90fd7b70b7e52c133197aeb5ca8da2d19c3dfb
22447887 <49071263+22447887@users.noreply.github.com>
2022-09-25 00:22:26 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/yuyeh/springbootmall/controller/ProductController.java
+++ src/main/java/com/yuyeh/springbootmall/controller/ProductController.java
@@ -35,4 +35,21 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+
+    @PutMapping("products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+
+        Product product = productService.getProductById(productId);
+
+        if(product == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        productService.updateProduct(productId, productRequest);
+
+        Product updateProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
 }
